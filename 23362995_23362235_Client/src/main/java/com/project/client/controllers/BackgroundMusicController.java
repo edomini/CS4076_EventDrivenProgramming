da57213de48407fbcb56e7837dfdfb34ef529f67
edomini <23362235@studentmail.ul.ie>
2025-03-29 21:02:58 +0000

    fixed music filepath

--- 23362995_23362235_Client/src/main/java/com/project/client/controllers/BackgroundMusicController.java
+++ 23362995_23362235_Client/src/main/java/com/project/client/controllers/BackgroundMusicController.java
@@ -3,14 +3,15 @@ package com.project.client.controllers;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import java.io.File;
+import java.net.URL;
 
 public class BackgroundMusicController {
     private static BackgroundMusicController instance;
     private MediaPlayer mediaPlayer;
 
     private BackgroundMusicController() {
-        String musicFile = "src/main/resources/music/sunnydays.mp3";
-        Media media = new Media(new File(musicFile).toURI().toString());
+        URL musicFile = getClass().getResource("/music/sunnydays.mp3");
+        Media media = new Media(musicFile.toString());
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
     }
