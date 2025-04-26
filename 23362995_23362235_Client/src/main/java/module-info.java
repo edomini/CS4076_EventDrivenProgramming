e6437a8af1ad115096e6be3fee6ccbd105956723
edomini <23362235@studentmail.ul.ie>
2025-04-26 16:48:44 +0100

    final version, works on netbeans!!

--- 23362995_23362235_Client/src/main/java/module-info.java
+++ 23362995_23362235_Client/src/main/java/module-info.java
@@ -5,5 +5,6 @@ module com.project.client {
     requires javafx.media;
 
     opens com.project.client.controllers to javafx.fxml;
+    opens com.project.client to javafx.fxml;
     exports com.project.client;
 }
